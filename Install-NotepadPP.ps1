8214b15f89bf417e2c5fcd4b78b59293f8b54755
AR Wiley <98679479+AR-Wiley@users.noreply.github.com>
2026-05-13 16:29:50 -0500

    Refactor Install-NotepadPP function structure

--- Install-NotepadPP.ps1
+++ Install-NotepadPP.ps1
@@ -1,13 +1,12 @@
-﻿function Install-NotepadPP {
 
-    #Confirm running as admin
-
-    if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-        Write-Host "Please run this script as Administrator."
-        return 
-    } else {
-        Write-Host "Running as Administrator."
-    }
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "Please run this script as Administrator."
+    return 
+} else {
+    Write-Host "Running as Administrator."
+}
+ 
+ function Install-NotepadPP {
   
    $notepadPPPath = "C:\Program Files\Notepad++\notepad++.exe"
 
